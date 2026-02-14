838a97cc1c6558edea564f473056f5a694ba7781
JackIzumihara <1551500700@k12.hi.us>
2026-02-14 09:50:29 -1000

    Update script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function programinitialize(){
+    console.log("********************");
+    console.log("PROGRAM INITIALIZED");
+    console.log("********************");
+};
+
+function program1() {
+    console.log(" ");
+    console.log("$5 is worth 20 quarters");
+    console.log(" ");
+    console.log("$10 is worth 40 quarters");
+    console.log(" ");
+};
+
+function program2() {
+    console.log(" ");
+    console.log("$2 is worth 8 quarters");
+    console.log(" ");
+};
+
+function programend() {
+    console.log(" ");
+    console.log("$0 is worth 0 quarters");
+};
+
+programinitialize();
+program1();
+programinitialize();
+program2();
+programinitialize();
+programend();
